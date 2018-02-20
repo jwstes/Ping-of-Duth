53707ff283600d620258ceba63ed8fb9d9a40d9a
jwstes <jwstes@gmail.com>
2018-02-20 23:00:56 +0800

    Project Files

---
+++ Ping of Duth/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Ping_of_Duth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Ping = new System.Windows.Forms.Button();
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_Ping
+            // 
+            this.btn_Ping.Font = new System.Drawing.Font("Work Sans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ping.Location = new System.Drawing.Point(12, 96);
+            this.btn_Ping.Name = "btn_Ping";
+            this.btn_Ping.Size = new System.Drawing.Size(58, 23);
+            this.btn_Ping.TabIndex = 0;
+            this.btn_Ping.Text = "Ping";
+            this.btn_Ping.UseVisualStyleBackColor = true;
+            this.btn_Ping.Click += new System.EventHandler(this.btn_Ping_Click);
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Font = new System.Drawing.Font("Work Sans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ip.Location = new System.Drawing.Point(12, 26);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(141, 23);
+            this.txt_ip.TabIndex = 1;
+            this.txt_ip.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Work Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Idle..";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Amount of Pings";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "10000";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(118, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Unlimited Pings";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 147);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ip);
+            this.Controls.Add(this.btn_Ping);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PoD - jwstes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Ping;
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Ping of Duth/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Ping_of_Duth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static IPAddress ip = IPAddress.None;
+        static Ping p = new Ping();
+        static PingReply reply;
+        static int length;
+
+        private void btn_Ping_Click(object sender, EventArgs e)
+        {
+            ip = IPAddress.Parse(txt_ip.Text);
+
+            if (checkBox1.Checked)
+            {
+                while (true)
+                {
+                    reply = p.Send(ip, 1000);
+                }
+            }
+            else
+            {
+                length = Convert.ToInt32(textBox1.Text);
+                for (int i = 0; i <= length; i++)
+                {
+                    reply = p.Send(ip, 1000);
+                    label3.Text = "Pings Done " + i.ToString();
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("The program may... lag? if you set a number above 10k pings. As for the unlimited pings, this program will surely lag");
+
+            
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+
+            if (checkBox1.Checked == false)
+            {
+                textBox1.Visible = true;
+            }
+
+        }
+
+
+    }
+}
